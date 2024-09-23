e4fb8c15b8dbb66c8f5ed6cc0537e174864e0678
gayeong718 <rkdud718718@gmail.com>
2024-09-24 01:05:08 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 383 ms, Memory: 65,336 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -1,25 +1,26 @@
-def dfs(x, y, path):
-    dr = [1, -1, 0, 0]
-    dc = [0, 0, 1, -1]
-
-    if len(path) == 7:
-        result.add(path)
+def dfs(step, row, col, number):
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    
+    number += mapp[row][col]
+    
+    if step == 6:
+        result.append(number)
         return
+    
+    for dx, dy in directions:
+        new_row, new_col = row + dx, col + dy
+        if 0 <= new_row < 4 and 0 <= new_col < 4:
+            dfs(step + 1, new_row, new_col, number)
 
-    for d in range(4):
-        nr = x + dr[d]
-        nc = y + dc[d]
-        if 0 <= nr < 4 and 0 <= nc < 4:
-            dfs(nr, nc, path+arr[nr][nc])
-
-
-T = int(input())
+T = int(input())  
 for t in range(T):
-    arr = [list(input().split()) for i in range(4)]
-    result = set()
-
-    for i in range(4):
-        for j in range(4):
-            dfs(i, j, arr[i][j])
-
-    print(f'#{t+1} {len(result)}')
+    mapp = [input().split() for _ in range(4)]
+    
+    result = []  
+    
+    for x in range(4):
+        for y in range(4):
+            dfs(0, x, y, "")
+    
+    answer = len(set(result))  
+    print(f'#{t+1} {answer}')
